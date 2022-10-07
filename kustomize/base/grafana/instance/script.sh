768350112c4132ea5756e59f2f85001d7775e0ec
Jason Ross <jaross@redhat.com>
2022-10-07 16:48:49 -0500

    add cronjob for manageing bound token refresh

---
+++ kustomize/base/grafana/instance/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TOKEN=$(cat /var/run/secrets/tokens/thanos-token)
+
+PATCH="[{\"op\": \"replace\", \"path\":\"/spec/datasources/0/secureDataJson/httpHeaderValue1\", \"value\": \"Bearer $TOKEN\"}]"
+
+
+CURLOPTS="-H 'Content-Type: application/json-patch+json' -H 'Authorization: Bearer $TOKEN' -X PATCH --data $PATCH"
+NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
+PATH="https://kubernetes.default.svc/ais/integreatly.org/v1alpha1/namespaces/$NAMESPACE/grafanadatasources/thanos/
+
+curl -k $CURLOPTS $PATH
