fa917d10f5b7a061f00e7ceed3c16810f4be8232
Jason Ross <jaross@redhat.com>
2022-10-10 14:42:30 -0500

    add curl

--- kustomize/base/grafana/instance/script.sh
+++ kustomize/base/grafana/instance/script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+microdnf install -y curl
 
 TOKEN=$(cat /var/run/secrets/tokens/thanos-token)
 
@@ -7,6 +8,6 @@ PATCH="[{\"op\": \"replace\", \"path\":\"/spec/datasources/0/secureDataJson/http
 
 CURLOPTS="-H 'Content-Type: application/json-patch+json' -H 'Authorization: Bearer $TOKEN' -X PATCH --data $PATCH"
 NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
-PATH="https://kubernetes.default.svc/ais/integreatly.org/v1alpha1/namespaces/$NAMESPACE/grafanadatasources/thanos/
+PATH="https://kubernetes.default.svc/ais/integreatly.org/v1alpha1/namespaces/$NAMESPACE/grafanadatasources/thanos/"
 
 curl -k $CURLOPTS $PATH
