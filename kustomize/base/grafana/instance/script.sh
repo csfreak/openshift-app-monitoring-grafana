e826afb3251dd45903aa8f5117019d50cdbdf4b7
Jason Ross <jaross@redhat.com>
2022-12-12 15:07:07 -0500

    update token

--- kustomize/base/grafana/instance/script.sh
+++ kustomize/base/grafana/instance/script.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-set +x
+
 TOKEN=$(cat /var/run/secrets/tokens/thanos-token)
+PATCH="'[{\"op\": \"replace\", \"path\":\"/spec/datasources/0/secureDataJson/httpHeaderValue1\", \"value\": \"Bearer ${TOKEN}\"}]'"
 
-PATCH="'[{\"op\": \"replace\", \"path\":\"/spec/datasources/0/secureDataJson/httpHeaderValue1\", \"value\": \"Bearer $TOKEN\"}]'"
-
-
-CURLOPTS="-H 'Content-Type: application/json-patch+json' -H 'Authorization: Bearer $TOKEN' -X PATCH --data $PATCH"
-NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
-PATH="https://kubernetes.default.svc/ais/integreatly.org/v1alpha1/namespaces/$NAMESPACE/grafanadatasources/thanos/"
-
-/usr/bin/curl -k $CURLOPTS $PATH
+set +x
+oc patch grafanadatasources thanos -n $(cat /var/run/secrets/token/serviceaccount/namespace) --type json --patch ${PATCH}
